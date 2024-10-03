71575a4872710ff7d5a60103f7fd37107d35f0d3
Rakesh <justsineup4321@gmail.com>
2024-10-04 03:07:48 +0530

    Finish cource but not much is implemented here

--- models/user-model.js
+++ models/user-model.js
@@ -10,17 +10,19 @@ const userSchema = mongoose.Schema({
     },
     email: String,
     password: String,
-    cart: {
-        type: Array, 
-        default: []
-    },
+    cart: [
+        {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: "product",
+        },
+    ],
     // isadmin: Boolean,
     orders: {
         type: Array, 
         default: []
     },
     contact: Number,
-    picture: String
+    picture: String,
 })
 
 
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,45 @@ const express = require('express')
 const router = express.Router()
 const isLoggedIn = require('../middlewares/isLoggedIn')
 const productModel = require('../models/product-model')
+const userModel = require('../models/user-model')
 
 router.get("/", function(req, res){
     let error = req.flash("error")
-    res.render("index", { error })
+    res.render("index", { error, loggedin: false })
 })
  
 router.get("/shop", isLoggedIn, async function(req, res){
     let products = await productModel.find()
-    res.render("shop", { products })
+    let success = req.flash("success")
+    res.render("shop", { products, success })
+})
+
+// ➡️ Give functional the add-to-cart icon...
+router.get("/addtocart/:productid", isLoggedIn, async function(req, res){
+    let user = await userModel.findOne({ email: req.user.email})
+
+    // Check if the user exists
+    if (!user) {
+        req.flash("error", "User not found")
+        return res.redirect("/shop")
+    }
+
+    // Add the product to the user's cart
+    user.cart.push(req.params.productid)
+    await user.save()
+    req.flash("success", "Added to cart")
+    res.redirect("/shop")
+})
+
+
+
+// ➡️ Give functionality to cart...
+router.get("/cart", isLoggedIn, async function(req, res){
+    let user = await userModel.findOne({email: req.user.email}).populate("cart")
+
+    const bill = (Number(user.cart[0].price)+20)-Number(user.cart[0].discount)
+
+    res.render("cart", { user , bill})
 })
 
 
